b216c21150aa25b8ebc78b762198abd41fde32d8
jonathancarbol <43402855+jonathancarbol@users.noreply.github.com>
2019-04-03 11:01:45 +0200

    Small changes in code and some graphical changed

--- src/recipesearch/RecipeBackendController.java
+++ src/recipesearch/RecipeBackendController.java
@@ -1,4 +1,40 @@
 package recipesearch;
 
+import se.chalmers.ait.dat215.lab2.Recipe;
+import se.chalmers.ait.dat215.lab2.SearchFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeBackendController {
+
+    SearchFilter searchFilter;
+
+
+
+
+    public List<Recipe> getRecipe(){
+
+        return new ArrayList<>();
+    }
+
+    public void setCuisine(String Cuisine){
+        searchFilter.setCuisine(Cuisine);
+    }
+
+    public void setMainIngredient(String mainIngredient){
+        searchFilter.setMainIngredient(mainIngredient);
+    }
+
+    public void setDifficulty(String difficulty){
+        searchFilter.setDifficulty(difficulty);
+    }
+
+    public void setMaxPrice(int maxPrice){
+        searchFilter.setMaxPrice(maxPrice);
+    }
+
+    public void setMaxTime(int maxTime){
+        searchFilter.setMaxTime(maxTime);
+    }
 }
--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -2,15 +2,19 @@
 package recipesearch;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import se.chalmers.ait.dat215.lab2.Recipe;
 import se.chalmers.ait.dat215.lab2.RecipeDatabase;
+import se.chalmers.ait.dat215.lab2.SearchFilter;
 
 
 public class RecipeSearchController implements Initializable {
 
     RecipeDatabase db = RecipeDatabase.getSharedInstance();
-    
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 

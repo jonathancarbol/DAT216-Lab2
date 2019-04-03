941b91915b587a9d56afb13cde68de9ecb1c9b73
jonathancarbol <43402855+jonathancarbol@users.noreply.github.com>
2019-04-03 14:33:44 +0200

    Step 2: Implementation of Backend

--- src/recipesearch/RecipeBackendController.java
+++ src/recipesearch/RecipeBackendController.java
@@ -1,6 +1,7 @@
 package recipesearch;
 
 import se.chalmers.ait.dat215.lab2.Recipe;
+import se.chalmers.ait.dat215.lab2.RecipeDatabase;
 import se.chalmers.ait.dat215.lab2.SearchFilter;
 
 import java.util.ArrayList;
@@ -8,33 +9,39 @@ import java.util.List;
 
 public class RecipeBackendController {
 
-    SearchFilter searchFilter;
+    SearchFilter searchFilter = new SearchFilter(null,0,null,0,null);
+    RecipeDatabase db = RecipeDatabase.getSharedInstance();
 
 
 
 
     public List<Recipe> getRecipe(){
 
-        return new ArrayList<>();
+        return db.search(searchFilter);
     }
 
     public void setCuisine(String Cuisine){
         searchFilter.setCuisine(Cuisine);
+        getRecipe();
     }
 
     public void setMainIngredient(String mainIngredient){
         searchFilter.setMainIngredient(mainIngredient);
+        getRecipe();
     }
 
     public void setDifficulty(String difficulty){
         searchFilter.setDifficulty(difficulty);
+        getRecipe();
     }
 
     public void setMaxPrice(int maxPrice){
         searchFilter.setMaxPrice(maxPrice);
+        getRecipe();
     }
 
     public void setMaxTime(int maxTime){
         searchFilter.setMaxTime(maxTime);
+        getRecipe();
     }
 }

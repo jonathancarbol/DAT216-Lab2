15ad544fe9c72797888d513cc3391f55135d218b
jonathancarbol <43402855+jonathancarbol@users.noreply.github.com>
2019-04-03 16:03:21 +0200

    Steg 8: Koppla på kontroller

--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -9,7 +9,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.ait.dat215.lab2.Recipe;
 import se.chalmers.ait.dat215.lab2.RecipeDatabase;
@@ -25,7 +25,13 @@ public class RecipeSearchController implements Initializable {
     @FXML FlowPane resultFlowPane;
     @FXML ComboBox mainIngredientList;
     @FXML ComboBox kitchenList;
-
+    @FXML RadioButton difficultyAll;
+    @FXML RadioButton difficultyEasy;
+    @FXML RadioButton difficultyMedium;
+    @FXML RadioButton difficultyHard;
+    @FXML Spinner maxPriceSpinner;
+    @FXML Slider maxTimeSlider;
+    @FXML Label minuteLabel;
 
     private void updateRecipeList(){
         resultFlowPane.getChildren().clear();
@@ -46,6 +52,70 @@ public class RecipeSearchController implements Initializable {
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 rbc.setMainIngredient(newValue);
                 updateRecipeList();
+            }
+        });
+
+        kitchenList.getItems().addAll("Visa alla","Sverige","Grekland","Indien","Asien","Afrika","Frankrike");
+        kitchenList.getSelectionModel().select("Visa alla");
+        kitchenList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                rbc.setMainIngredient(newValue);
+                updateRecipeList();
+            }
+        });
+
+        ToggleGroup difficultyToggleGroup = new ToggleGroup();
+        difficultyAll.setToggleGroup(difficultyToggleGroup);
+        difficultyEasy.setToggleGroup(difficultyToggleGroup);
+        difficultyMedium.setToggleGroup(difficultyToggleGroup);
+        difficultyHard.setToggleGroup(difficultyToggleGroup);
+        difficultyAll.setSelected(true);
+        difficultyToggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                if(difficultyToggleGroup.getSelectedToggle() != null){
+                    RadioButton selected = (RadioButton) difficultyToggleGroup.getSelectedToggle();
+                    rbc.setDifficulty(selected.getText());
+                    updateRecipeList();
+                }
+            }
+        });
+
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,300,100,10);
+        maxPriceSpinner.setEditable(true);
+        maxPriceSpinner.setValueFactory(valueFactory);
+        maxPriceSpinner.valueProperty().addListener(new ChangeListener<Integer>() {
+            @Override
+            public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+                rbc.setMaxPrice(newValue);
+                updateRecipeList();
+            }
+        });
+        maxPriceSpinner.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+                if(newValue){
+
+                }else{
+                    Integer value = Integer.valueOf(maxPriceSpinner.getEditor().getText());
+                    rbc.setMaxPrice(value);
+                    updateRecipeList();
+                }
+
+            }
+        });
+
+        minuteLabel.setText("0");
+        maxTimeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if(newValue != null && !newValue.equals(oldValue) && !maxTimeSlider.isValueChanging()) {
+                    rbc.setMaxTime(newValue.intValue());
+                    updateRecipeList();
+                }
+                minuteLabel.setText(Integer.toString((int)Math.floor(maxTimeSlider.getValue())));
             }
         });
     }

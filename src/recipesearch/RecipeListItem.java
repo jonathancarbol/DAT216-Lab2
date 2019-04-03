f90f2a137c8d86fd93acb1d941adaca1071457ce
jonathancarbol <43402855+jonathancarbol@users.noreply.github.com>
2019-04-03 15:10:28 +0200

    Steg 7: Skapa listitems dynamiskt

---
+++ src/recipesearch/RecipeListItem.java
@@ -0,0 +1,33 @@
+package recipesearch;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.ait.dat215.lab2.Recipe;
+
+import java.io.IOException;
+
+public class RecipeListItem extends AnchorPane {
+    private RecipeSearchController parentController;
+    private Recipe recipe;
+
+    @FXML public Label recipeItemName;      // Hämtas via recipe
+    @FXML public ImageView recipeImage;
+
+    public RecipeListItem(Recipe recipe, RecipeSearchController recipeSearchController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("recipe_listitem.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        this.recipe = recipe;
+        this.parentController = recipeSearchController;
+    }
+}
--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -4,7 +4,10 @@ package recipesearch;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
 import se.chalmers.ait.dat215.lab2.Recipe;
 import se.chalmers.ait.dat215.lab2.RecipeDatabase;
 import se.chalmers.ait.dat215.lab2.SearchFilter;
@@ -13,11 +16,25 @@ import se.chalmers.ait.dat215.lab2.SearchFilter;
 public class RecipeSearchController implements Initializable {
 
     RecipeDatabase db = RecipeDatabase.getSharedInstance();
+    RecipeBackendController rbc = new RecipeBackendController();
+    private List<Recipe> recipes;
 
+    @FXML FlowPane resultFlowPane;
+
+
+
+    private void updateRecipeList(){
+        resultFlowPane.getChildren().clear();
+        recipes = rbc.getRecipe();
+        for(Recipe r:recipes) {
+            resultFlowPane.getChildren().add(new RecipeListItem(r, this));
+        }
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        updateRecipeList();
     }
 
 }
